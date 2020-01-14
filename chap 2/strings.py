9dfd567a95c1a01e93f516e84a5c8406d61bffad
Diallo Mamadou Lamarana <lyve.diallo@gmail.com>
2020-01-14 10:01:35 +0000

    working on string datas

---
+++ chap 2/strings.py
@@ -0,0 +1,16 @@
+name = "mamadou Lamarana DIALLO"
+
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+print("Hello " + name.title())
+
+print("Languages : \n\tPython\n\tC\n\tJavascript")
+
+string = "    I have a whitespace at the begining"
+print(string)
+print(string.strip())
+
+age = 23
+print("Happy " + str(age) + "rd Birthday!!")
